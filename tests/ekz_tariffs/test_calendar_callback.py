e0529414ab5daab4d8fa3ca2dc9cd1a1728d6471
Felix Schmidt <fs@deepsports.io>
2025-12-28 22:53:06 +0100

    fix unit tests

--- tests/ekz_tariffs/test_calendar_callback.py
+++ tests/ekz_tariffs/test_calendar_callback.py
@@ -4,20 +4,18 @@ import datetime as dt
 from unittest.mock import patch
 
 import pytest
-from custom_components.ekz_tariffs.const import EVENT_TARIFF_START, EVENT_TYPE
 from homeassistant.core import HomeAssistant
 from homeassistant.util import dt as dt_util
 from pytest_homeassistant_custom_component.common import (
-    async_capture_events,
-    async_fire_time_changed,
-)
+    async_capture_events, async_fire_time_changed)
+
+from custom_components.ekz_tariffs.const import EVENT_TARIFF_START, EVENT_TYPE
 
 
 @pytest.mark.asyncio
 async def test_calendar_fires_callback_event_on_start(
     hass: HomeAssistant, mock_config_entry
 ):
-    await hass.config.async_set_time_zone("Europe/Zurich")
     mock_config_entry.add_to_hass(hass)
 
     # Choose a start in the future relative to patched "now"
@@ -45,7 +43,6 @@ async def test_calendar_fires_callback_event_on_start(
         async_fire_time_changed(hass, start)
         await hass.async_block_till_done()
 
-    print(events)
     assert len(events) >= 1
     assert events[-1].data["type"] == EVENT_TARIFF_START
     assert events[-1].data["price_chf_per_kwh"] == 0.30
